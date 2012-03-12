4f17a753b72ed5c07e8a07acf08f523aa940daff
Wim Cosyn <wim.cosyn@ugent.be>
2012-03-12 14:35:04 +0000

    updated some pow(-1 again

--- Tools/newcoef.cpp
+++ Tools/newcoef.cpp
@@ -40,8 +40,8 @@ Newcoef::Newcoef( int n1, int l1, int two_j1, int two_mj1, int two_t1, int n2, i
 	      int J = two_J/2;
 	      for( int MJ = -J; MJ <= J; MJ++)
 	      {
-		      double threej1 = pow( -1, ( two_j1- two_j2+ 2*MJ)/2 ) * sqrt( two_J+1) * gsl_sf_coupling_3j( two_j1, two_j2, two_J, two_mj1, two_mj2, 2*MJ ); 
-		      double threej2 = pow( -1, j-L+MJ) * sqrt( 2*J+1) * gsl_sf_coupling_3j( 2*j, 2*L, 2*J, 2*mj, 2*ML, -2*MJ);
+		      double threej1 = pow( -1., ( two_j1- two_j2+ 2*MJ)/2 ) * sqrt( two_J+1) * gsl_sf_coupling_3j( two_j1, two_j2, two_J, two_mj1, two_mj2, 2*MJ ); 
+		      double threej2 = pow( -1., j-L+MJ) * sqrt( 2*J+1) * gsl_sf_coupling_3j( 2*j, 2*L, 2*J, 2*mj, 2*ML, -2*MJ);
 		      double isospin = sqrt( 2*T + 1.) * pow(-1., MT) * gsl_sf_coupling_3j( 1, 1, 2*T, two_t1, two_t2, -2*MT);
 		      for( int Lambda = fabs(l1-l2); Lambda <= l1+l2; Lambda++)
 		      {
@@ -52,7 +52,7 @@ Newcoef::Newcoef( int n1, int l1, int two_j1, int two_mj1, int two_t1, int n2, i
 			      }
 			      double ninej = sqrt(two_j1+1) * sqrt(two_j2+1) * sqrt(2*S+1) * sqrt(2*Lambda+1) 
 			      * gsl_sf_coupling_9j(2*l1, 1, two_j1, 2*l2, 1, two_j2, 2*Lambda, 2*S, 2*J);
-			      double sixj = sqrt(2*Lambda+1) * sqrt(2*j+1) * pow(-1, j+Lambda+S+L) * gsl_sf_coupling_6j(2*j, 2*L, 2*J, 2*Lambda, 2*S, 2*l);
+			      double sixj = sqrt(2*Lambda+1) * sqrt(2*j+1) * pow(-1., j+Lambda+S+L) * gsl_sf_coupling_6j(2*j, 2*L, 2*J, 2*Lambda, 2*S, 2*l);
 			      value += factor* ninej* sixj*threej1* threej2* isospin* 2*moshme;
 		      }
 	      }
