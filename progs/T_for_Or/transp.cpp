7ca4a0e760976b8cbbf76bd86c769ab2960980d5
Wim Cosyn <wim.cosyn@ugent.be>
2012-09-14 10:49:29 +0000

    updated transparency program with new integrators....

--- progs/T_for_Or/transp.cpp
+++ progs/T_for_Or/transp.cpp
@@ -27,8 +27,9 @@ using namespace std;
 
 void intPm(const double pm, double *results, va_list ap);
 void intcosth(const double costh, double *results, va_list ap);
-void getBound(double &high, double &low, MeanFieldNucleusThick *pnucleus, double Q2, double omega, int shell);
-void adap_intPm(numint::vector_d &, double pm,Cross* pObs, MeanFieldNucleusThick *pNucleus, TElectronKinematics *elec);
+void getBound(double &high, double &low, MeanFieldNucleusThick &nucleus, double Q2, double omega, int shell);
+void adap_intPm(numint::vector_d &, double costhetacm,Cross* pObs, MeanFieldNucleusThick *pNucleus, TElectronKinematics *elec,
+		double Q2, double omega, int current, double *cthmax);
 
 
 //run ./transp [nucleon] [Q2,GeV^2] [Ein, MeV] [thetae, degr] [thickness] [sharedir]
@@ -50,61 +51,113 @@ int main(int argc, char *argv[])
   MeanFieldNucleusThick Nucleus(atoi(argv[1]),homedir);
   //TKinematics2to2 kin("","",.getMassA(),Carbon.getMassA_min_proton(),MASSP,"qsquared:wlab:pklab",atof(argv[1]),atof(argv[2]),atof(argv[3]));
   TElectronKinematics *elec = TElectronKinematics::CreateWithBeamEnergy(Ein);
-  Cross obs(*elec,&Nucleus,prec,homedir, userset, screening);
+  Cross obs(*elec,&Nucleus,prec, 2, homedir, userset, screening);
 
+  double total[5];
+  for(int i=0;i<5;i++) total[i]=0.;
   
-//   for(int i=0;i<5;i++) total[i]=0.;
-//   
-//   for(int shell=0;shell</*Nucleus.getPLevels()*/1;shell++){
-//     double low=-1.,high=1.;
-//     getBound(high,low,&Nucleus,Q2,omega,shell);
-//     double results[5];
-//     double pestimate=0.;
-//     rombergerN(intPm,-1.,low,5, results,1.E-02,3,10,&pestimate,&obs,elec,&Nucleus,Q2,omega,shell, 
-// 	       thick,current);
-//     for(int j=0;j<5;j++) total[j]+=results[j];
-//     
-//     cout << Q2/1.E06 << " " << Ein << " " << shell << " " << results[0]/results[4] << " " << 
-// 	    results[1]/results[4] << " " << results[2]/results[4] << " " << results[3]/results[4] << endl;
-//   }
-//   cout << endl;
-//     cout << Q2/1.E06 << " " << Ein << " " << total[0]/total[4] << " " << 
-// 	    total[1]/total[4] << " " << total[2]/total[4] << " " << total[3]/total[4] << endl;
-
-  vector<double> totalcross(5,0.) 
+  for(int shell=0;shell<Nucleus.getPLevels();shell++){
+    double low=-1.,high=1.;
+    getBound(high,low,Nucleus,Q2,omega,shell);
+    double results[5];
+    double pestimate=0.;
+    rombergerN(intPm,-1.,low,5, results,1.E-02,3,10,&pestimate,&obs,elec,&Nucleus,Q2,omega,shell, 
+	       thick,current);
+    for(int j=0;j<5;j++) total[j]+=results[j];
+    
+    cout << Q2/1.E06 << " " << Ein << " " << shell << " " << results[0]/results[4] << " " << 
+	    results[1]/results[4] << " " << results[2]/results[4] << " " << results[3]/results[4] << endl;
+  }
+  cout << endl;
+    cout << Q2/1.E06 << " " << Ein << " " << total[0]/total[4] << " " << 
+	    total[1]/total[4] << " " << total[2]/total[4] << " " << total[3]/total[4] << endl;
+  exit(1);
+	    
+  vector<double> totalcross(5,0.); 
+  double cthmax[Nucleus.getPLevels()];
+  double cthmin[Nucleus.getPLevels()];
+  double max=-1.;
+  for(int shell=0;shell<Nucleus.getPLevels();shell++) {
+    cthmax[shell]=1.; cthmin[shell]=-1.;
+    getBound(cthmax[shell],cthmin[shell],Nucleus,Q2,omega,shell);
+//     cout << cthmax[shell] << endl;
+    if(max<cthmax[shell]) max=cthmax[shell];
+  }
+//   cout << max << endl;
   struct Ftor {
 
     static void exec(const numint::array<double,1> &x, void *param, numint::vector_d &ret) {
       Ftor &p = * (Ftor *) param;
-      p.f(ret,x[0],p.pObs,p.pNucleus,p.elec);
+      p.f(ret,x[0],p.pObs,p.pNucleus,p.elec,p.Q2,p.omega,p.current,p.cthmax);
     }
     Cross *pObs;
     MeanFieldNucleusThick *pNucleus;
     TElectronKinematics *elec;
-    void (*f)(numint::vector_d &, double pm,Cross* pObs, MeanFieldNucleusThick *pNucleus, TElectronKinematics *elec);
+    double Q2;
+    double omega;
+    int current;
+    double *cthmax;
+    void (*f)(numint::vector_d &, double pm,Cross* pObs, MeanFieldNucleusThick *pNucleus, TElectronKinematics *elec,
+	  double Q2, double omega, int current, double *cthmax);
+
   };
 
   Ftor F;
-  F.pObs = &Obs;
+  F.pObs = &obs;
   F.pNucleus = &Nucleus;
   F.elec=elec;
+  F.Q2=Q2;
+  F.omega=omega;
+  F.current=current;
+  F.cthmax=cthmax;
 
   numint::mdfunction<numint::vector_d,1> mdf;
   mdf.func = &Ftor::exec;
   mdf.param = &F;
 
   unsigned neval = 0;
-  numint::array<double,1> lower = {{0.}};
-  numint::array<double,1> upper = {{1.,1.,2.*PI}};
-  
-  double SIGNIF = 1.E-05;
+  numint::array<double,1> lower = {{-1.}};
+  numint::array<double,1> upper = {{max}};
   
   
+  F.f=adap_intPm;
+  unsigned count=0;
+  numint::cube_romb(mdf,lower,upper,1.E-20,1.E-03,totalcross,count,0);
+//   numint::cube_adaptive(mdf,lower,upper,1.E-20,1.E-03,totalcross,count,0);
+  
+    cout << Q2/1.E06 << " " << Ein << " " << totalcross[0]/totalcross[4] << " " << 
+	    totalcross[1]/totalcross[4] << " " << totalcross[2]/totalcross[4] << " " << totalcross[3]/totalcross[4] << " " << totalcross[4] << " " << count << endl;
   
   
   delete elec;
   return 0;
 }
+
+void adap_intPm(numint::vector_d & results, double costhetacm,Cross* pObs, MeanFieldNucleusThick *pNucleus, TElectronKinematics *elec,
+		double Q2, double omega, int current, double *cthmax){
+		  
+results=numint::vector_d(5,0.);
+for(int shell=0;shell<pNucleus->getPLevels();shell++) {
+  if(costhetacm<cthmax[shell]){
+    TKinematics2to2 kin("","",pNucleus->getMassA(),
+			pNucleus->getMassA_min_proton()+pNucleus->getExcitation()[shell],
+			MASSP,"qsquared:wlab:costhkcm",Q2,omega,costhetacm);
+    double pm=kin.GetPklab();
+    if(!kin.IsPhysical()){
+      for(int i=0;i<5;i++) results[i]=0.;
+      cout << "bla " << pm << endl;
+    }
+    numint::vector_d cross=numint::vector_d(5,0.);
+    pObs->getAllDiffCross(cross,kin,current,shell,1,0.);
+    for(int i=0;i<5;++i) results[i]+=cross[i];
+    cout << costhetacm << " " << pm << " " << shell << " " << results[0] << endl;
+  }
+  
+}
+  
+
+}
+
 
 void intPm(const double costhcm, double *results, va_list ap){
   Cross* p_obs = va_arg(ap,Cross*);
@@ -129,23 +182,21 @@ void intPm(const double costhcm, double *results, va_list ap){
     return;
   }
   // kin.Print();
-  results[0]=p_obs->getDiffCross(kin,  current, thick, 0, 0, 0, shell, 0.);
-  results[1]=p_obs->getDiffCross(kin,  current, thick, 1, 0, 0, shell, 0.);
-  results[2]=p_obs->getDiffCross(kin,  current, thick, 0, 1, 0, shell, 0.);
-  results[3]=p_obs->getDiffCross(kin,  current, thick, 1, 1, 0, shell, 0.);
-  results[4]=p_obs->getDiffCross(kin,  current, thick, 0, 0, 1, shell, 0.);
+    numint::vector_d cross=numint::vector_d(5,0.);
+    p_obs->getAllDiffCross(cross,kin,current,shell,1,0.);
+    for(int i=0;i<5;++i) results[i]=cross[i];
   cout << pm << " " << kin.IsPhysical() << " " << acos(kin.GetCosthYlab())*RADTODEGR << " " << kin.GetCosthYlab() << " " << acos(kin.GetCosthklab())*RADTODEGR << " " << kin.GetCosthklab() <<" " << results[0] << " " << results[1] << " " << results[4] << endl;
   return;
 }
 
-void getBound(double &high, double &low, MeanFieldNucleusThick *pnucleus, double Q2, double omega, int shell){
-  TKinematics2to2 kin("","",pnucleus->getMassA(),
-		      pnucleus->getMassA_min_proton()+pnucleus->getExcitation()[shell],
+void getBound(double &high, double &low, MeanFieldNucleusThick &nucleus, double Q2, double omega, int shell){
+  TKinematics2to2 kin("","",nucleus.getMassA(),
+		      nucleus.getMassA_min_proton()+nucleus.getExcitation()[shell],
 		      MASSP,"qsquared:wlab:costhkcm",Q2,omega,(high+low)/2.);
   double pm=kin.GetPklab();
   if(pm<300.) low=(high+low)/2.;
   else high=(high+low)/2.;
   if((high-low)<1.E-04) { return;}
-  else getBound(high,low,pnucleus,Q2,omega,shell);  
+  else getBound(high,low,nucleus,Q2,omega,shell);  
 }
 
