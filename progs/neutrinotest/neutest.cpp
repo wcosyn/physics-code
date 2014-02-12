5811094ba8f79abb1a40f35aafb49fd2b154a091
Wim Cosyn <wim.cosyn@ugent.be>
2014-02-12 16:53:59 +0000

    homedir parameter

--- progs/neutrinotest/neutest.cpp
+++ progs/neutrinotest/neutest.cpp
@@ -313,7 +313,7 @@ int main(int argc, char *argv[])
   
 //   double omega=Q2/(2.*MASSP*Bjx);
   
-  string homedir="/home/wim/Code/share";
+  string homedir=argv[4];//"/home/wim/Code/share";
 
   MeanFieldNucleusThick Nucleus(nucleus,homedir);
   TLeptonKinematics *lepton = TLeptonKinematics::CreateWithCosScatterAngle(TLeptonKinematics::muon,costhetamu);
