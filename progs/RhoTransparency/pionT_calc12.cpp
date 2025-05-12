0a4c38e7d0e48bac1934f1de4f9cca3c95c136ae
wcosyn <wim.cosyn@ugent.be>
2025-05-12 12:21:16 -0400

    forgot to commit everything in the last commit

---
+++ progs/RhoTransparency/pionT_calc12.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+#include <PionTCross.hpp>
+#include <Utilfunctions.hpp>
+#include <string>
+
+
+
+int main(int argc, char *argv[])
+{
+
+    string arg_names[5]={"exec name", "nucleus name","beam energy [GeV]","scattered energy [GeV]","scattered angle [deg]"};
+    if(argc!=5){
+        cout << "Usage: " << argv[0] << " <nucleus name> <beam energy [GeV]> <scattered energy [GeV]> <scattered angle [deg]>" << endl;
+        return 1;
+    }
+    std::cout << "Called from file: " << __FILE__ << std::endl;
+    Bookkeep(argc,argv,arg_names);  
+
+
+
+    string nucleus_name=argv[1];
+    double Ebeam  = atof(argv[2])*1.E03;  //Beam energy in GeV, converted to MeV
+    double E_out = atof(argv[3])*1.E03;  // Scattered electron energy in GeV, converted to MeV
+    double theta_e = atof(argv[4])*DEGRTORAD; // Scattered electron angle in degrees input converted to radians
+    
+    double Q2 = 2.*Ebeam*E_out*(1.-cos(theta_e)); //Q2 in MeV^2
+
+    PionTCross pionT_object = PionTCross(MeanFieldNucleus::TypeNames.at(nucleus_name),
+                                  400.,HOMEDIR,0,0.,1.E-05,2,20000);
+    double *results = new double[pionT_object.getNrofcross()];
+    pionT_object.getCross(results, Ebeam,E_out,theta_e);
+
+    cout << Q2*1.E-06 << " ";
+    for(int i=0;i<pionT_object.getNrofcross();i++){
+        cout << results[i] << " ";
+    }
+    cout << endl;
+    delete [] results;
+    return 0;
+}
